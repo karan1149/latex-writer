Weighting fix

image_utils.py
@@ -65,7 +65,7 @@ def get_math_dataset():
 	test_y = y[:int(y.shape[0] / 10)]
 	train_y = y[int(y.shape[0] / 10):]
 
-	classes_min = min(classes_counts)
+	classes_min = sum(classes_counts)
 	classes_distribution = np.array(classes_counts) / classes_min
 
 	print("distribution:", classes_distribution)